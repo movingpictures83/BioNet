4a29c6fdb04e06ed490bb24a5523a25a3f10d378
Luis Vega <luis.m.vega.a@gmail.com>
2018-06-19 15:56:00 -0400

    Unit Test for Degree

--- Dijkstra/Dijkstra/Dijkstra.cpp
+++ Dijkstra/Dijkstra/Dijkstra.cpp
@@ -3,16 +3,44 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "../../BioNet.h"
 using std::cout;
 using std::endl;
+using std::fixed;
 
+bool UnitTest();
 int main()
 {
 	cout << "Rafa was here." << endl;
     cout << "Luis' commit\n";
 	cout << "Hello World!" << endl;
 	cout << "From Lian MO.";
-	cout << "Daniel's code was here...";
+	cout << "Daniel's code was here...\n";
+	
+	UnitTest();
 	return 0;
 }
 
+bool UnitTest()
+{
+	BioNet TestBio;
+	char posNeg = 1;
+	//Random Values
+	cout << " \t0\t1\t2\t3\t4\n";
+	for (int i{ 0 }; i < NETWORK_SIZE; i++)
+	{
+		cout << i << '\t';
+		for (int j{ 0 }; j < NETWORK_SIZE; j++)
+		{
+			TestBio.setEdge( i, j, ((i + j) % 2 == 0 ? -1 : 1) * (static_cast <float> (rand()) / static_cast <float> (RAND_MAX)));
+			cout.precision(2);
+			cout << fixed << TestBio.getEdge(i, j) << '\t';
+		}
+		cout << endl;
+	}
+	for (int i{ 0 }; i < NETWORK_SIZE; i++)
+	{
+		cout << "Element# " << i << ": " << TestBio.degree(i) << endl;
+	}
+	return true;
+}
