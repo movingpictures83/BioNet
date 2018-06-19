722b2ca53dd3b2919106417c671f7eb046159157
Howard Gil <howardbgil@gmail.com>
2018-06-19 10:53:24 -0400

    Added unit test

---
+++ Einstein/UnitTest/unittest1.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../../BioNet.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			BioNet b = BioNet();
+			for (int i = 0; i < NETWORK_SIZE; ++i)
+			{
+				for (int j = 0; j < NETWORK_SIZE; ++j)
+				{
+					float min = b.getMinWeight();
+					float max = b.getMaxWeight();
+
+					Assert::IsTrue(b.getEdge(i,j) >= min);
+					Assert::IsTrue(b.getEdge(i,j) <= max);
+				}
+			}
+			
+		}
+
+	};
+}
