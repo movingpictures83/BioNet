1b8c853daaf658c74f662b959e0778381838fbf9
suryapchand <schand@farelogix.com>
2018-06-21 15:19:36 -0400

    BioList files

---
+++ BioList.cpp
@@ -0,0 +1,19 @@
+#include "../BioNet/BioList.h"
+#include "../BioNet/BioNet.h"
+
+
+BioList::BioList(float weight, string index) {
+	head = new BioNode(weight, index, NULL);
+}
+
+BioList::~BioList() {
+	clear();
+}
+
+void BioList::clear() {}
+
+bool BioList::doSearch(string name, BioNode* start) {
+	if (start == NULL) return false;
+	else if (start->getName() == name) return true;
+	else return doSearch(name, start->getNext());
+}
