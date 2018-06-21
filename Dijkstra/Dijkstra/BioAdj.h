f53921774dbb6e72080a9ad17b7bcc5fd3f66cef
fxhuang71 <lian.mo@gmail.com>
2018-06-21 15:53:45 -0300

    BioAdj interface added

---
+++ Dijkstra/Dijkstra/BioAdj.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+using std::string;
+
+class BioAdj
+{
+protected:
+	string keyword;
+public:
+     BioAdj();
+	~BioAdj();
+	virtual void setEdge(int, int, float);
+	virtual void setEdge(string, string, float);
+	virtual void getEdge(int, int);
+	virtual void getEdge(string, string);
+	virtual void setNode(int, string);
+	virtual void getNode(int);
+};
+
