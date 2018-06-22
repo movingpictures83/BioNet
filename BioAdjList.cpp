0d755e71a242fa93269f6749bfb7789edff842ca
Rafael Zayas <rzayas@farelogix.com>
2018-06-22 14:37:36 -0300

    Implemented accesors for BioAdjList

--- BioAdjList.cpp
+++ BioAdjList.cpp
@@ -2,21 +2,20 @@
 
 void BioAdjList::setEdge(int x, int y, float w)
 {
-	auto * current = &network[x];
-	while (current)
-	{
-	
-	}
-	
+	network[x].setWeight(network[y].getName(), w);
 }
 
-void BioAdjList::setEdge(string, string, float)
+void BioAdjList::setEdge(string x, string y, float w)
 {
+	int i = 0;
+	while (network[i].getName() != x)
+		i++;
+	network[i].setWeight(y, w);
 }
 
-float BioAdjList::getEdge(int, int)
+float BioAdjList::getEdge(int x, int y)
 {
-	return 0.0f;
+	return network[x].getWeight(network[y].getName());
 }
 
 float BioAdjList::getEdge(string, string)
