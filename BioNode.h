6d4a299a7304b84e8cd9a2d2f224da5918c0856f
Rafael Zayas <rzayas@farelogix.com>
2018-06-22 14:21:24 -0300

    Adding weight accesors to BioNode

--- BioNode.h
+++ BioNode.h
@@ -13,5 +13,7 @@ public:
 	BioNode(float f, string i, BioNode* n) { weight = f; name = i; next = n; }
 	string getName() { return name; }
 	BioNode* getNext() { return next; }
+	void setWeight(float f) { weight = f; }
+	float getWeight() { return weight; }
 	void setNext(BioNode* n) { next->next = n; }
 };
