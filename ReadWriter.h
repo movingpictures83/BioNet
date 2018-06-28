a0aed610228060e1e1271f6f8587044ff37fa102
jiayilianwei <jiayilianwei@gmail.com>
2018-06-28 11:07:13 -0300

    Adding Doxygen style comments for IO classes

--- ReadWriter.h
+++ ReadWriter.h
@@ -3,8 +3,12 @@
 #include "Writer.h"
 
 namespace BioNet {
+	/**
+	Subclass of read writer that allows for an object to be use with all of the functionality 
+	of both classes
+	*/
 	class ReadWriter : public  Reader, public Writer {
-
+	
 	public:
 		ReadWriter(string p = IO::getDefaultPath()) : Reader(p), Writer(p) {}
 	};
