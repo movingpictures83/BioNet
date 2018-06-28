75479cea191d27492f0f59b31b9efdfa3e864ff4
Qing Gao <gqingee@gmail.com>
2018-06-28 14:37:13 -0300

    new project file

---
+++ Knuth/Knuth/GMLHandler.h
@@ -0,0 +1,47 @@
+#pragma once
+
+
+#include <string>
+#include <fstream>
+#include "Net.h"
+#include "Reader.h"
+#include "Writer.h"
+#include "File.h"
+#include <vector>
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::ios;
+using std::fixed;
+using std::endl;
+using std::vector;
+
+struct Node {
+	int id;
+	string label;
+};
+struct Edge {
+	int source;
+	int target;
+	double weight;
+};
+
+
+class GMLHandler : public FileHandler
+{
+private:
+	ifstream infile;
+	vector<Node> nodes;
+	vector<Edge> edges;
+
+public:
+	GMLHandler() { extension = "gml"; }
+	template <typename T>
+	static void doRead(BioNet<T>&, const string& fname);
+	template <typename T>
+	static void doWrite(BioNet<T>&, const string& fname);
+//	GMLHandler(string p = "") : Reader(p), Writer(p) {};
+//	~GMLHandler();
+//	string getDefaultExt() { return ".gml"; }
+};
+
