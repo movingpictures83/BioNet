2772441230768576ed2c676adbc532ad4ef500c3
fxhuang71 <lian.mo@gmail.com>
2018-06-28 10:13:11 -0300

    Changing the code to use BioNet namespace

---
+++ Turing/Turing/CSVHandler.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "CSVHandler.h"
+#include <fstream>
+#include <sstream>
+#include "..\..\BioNetException.h"
+
+using std::ifstream;
+using std::ios;
+using std::stringstream;
+
+
+vector <string> CSVHandler::split(const string &s, char delim) {
+	vector<string> result;
+	stringstream ss(s);
+	string item;
+	while (getline(ss, item, delim)) {
+		result.push_back(item);
+	}
+	return result;
+}
+
